e20568b0f43c8c3344cc617b9b4ba41b26564ffc
JoePerPaf <joel.persson@paf.com>
2022-07-29 10:51:52 +0200

    Updated Vec2

--- Vec2.h
+++ Vec2.h
@@ -11,17 +11,33 @@ public:
     T x;
     T y;
 
-    inline constexpr Vec2 operator+(const Vec2& rhs)const{ return {x + rhs.x, y + rhs.y}; }
-    inline constexpr Vec2& operator+=(const Vec2& rhs){ return *this = *this + rhs;}
+    inline constexpr const Vec2 operator+(const Vec2& rhs)const{ return {x + rhs.x, y + rhs.y}; }
+    inline constexpr const Vec2& operator+=(const Vec2& rhs){ return *this = *this + rhs;}
+    inline constexpr const Vec2 operator+(const int& rhs)const{return{x + rhs, y + rhs};}
+    inline constexpr const Vec2& operator+=(const int& rhs) {return *this = *this + rhs;}
+    inline constexpr const Vec2 operator+(const float& rhs)const{return{x + rhs, y + rhs};}
+    inline constexpr const Vec2& operator+=(const float& rhs) {return *this = *this + rhs;}
 
-    inline constexpr Vec2 operator-(const Vec2& rhs)const{ return {x - rhs.x, y - rhs.y}; }
-    inline constexpr Vec2& operator-=(const Vec2& rhs){ return *this = *this - rhs;}
+    inline constexpr const Vec2 operator-(const Vec2& rhs)const{ return {x - rhs.x, y - rhs.y}; }
+    inline constexpr const Vec2& operator-=(const Vec2& rhs){ return *this = *this - rhs;}
+    inline constexpr const Vec2 operator-(const int& rhs)const{return{x - rhs, y - rhs};}
+    inline constexpr const Vec2& operator-=(const int& rhs) {return *this = *this - rhs;}
+    inline constexpr const Vec2 operator-(const float& rhs)const{return{x - rhs, y - rhs};}
+    inline constexpr const Vec2& operator-=(const float& rhs) {return *this = *this - rhs;}
 
-    inline constexpr Vec2 operator*(const Vec2& rhs)const{ return {x * rhs.x, y * rhs.y}; }
-    inline constexpr Vec2& operator*=(const Vec2& rhs){ return *this = *this * rhs;}
-    
-    inline constexpr Vec2 operator/(const Vec2& rhs)const{ return {x / rhs.x, y / rhs.y}; }
-    inline constexpr Vec2& operator/=(const Vec2& rhs){ return *this = *this / rhs;}
+    inline constexpr const Vec2 operator*(const Vec2& rhs)const{ return {x * rhs.x, y * rhs.y}; }
+    inline constexpr const Vec2& operator*=(const Vec2& rhs){ return *this = *this * rhs;}
+    inline constexpr const Vec2 operator*(const int& rhs)const{return{x * rhs, y * rhs};}
+    inline constexpr const Vec2& operator*=(const int& rhs) {return *this = *this * rhs;}
+    inline constexpr const Vec2 operator*(const float& rhs)const{return{x * rhs, y * rhs};}
+    inline constexpr const Vec2& operator*=(const float& rhs) {return *this = *this * rhs;}
+
+    inline constexpr const Vec2 operator/(const Vec2& rhs)const{ return {x / rhs.x, y / rhs.y}; }
+    inline constexpr const Vec2& operator/=(const Vec2& rhs){ return *this = *this / rhs;}
+    inline constexpr const Vec2 operator/(const int& rhs)const{return{x / rhs, y / rhs};}
+    inline constexpr const Vec2& operator/=(const int& rhs) {return *this = *this / rhs;}
+    inline constexpr const Vec2 operator/(const float& rhs)const{return{x / rhs, y / rhs};}
+    inline constexpr const Vec2& operator/=(const float& rhs) {return *this = *this / rhs;}
 
     inline constexpr bool operator==(const Vec2& rhs) const {return x == rhs.x && y == rhs.y;}
     inline constexpr bool operator!=(const Vec2& rhs) const {return !(*this == rhs);}
